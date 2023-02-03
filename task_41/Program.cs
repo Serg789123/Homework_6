575678bc228130d2bfd8ab9a669f13824144bc41
Sergey <diuk@inbox.ru>
2023-02-03 16:05:22 +0300

    коммит Методов 2

--- task_41/Program.cs
+++ task_41/Program.cs
@@ -3,14 +3,27 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
+
 Console.Write("Введите количество элементов массива:\t ");
 int elementsCount = int.Parse(Console.ReadLine());
 int[] myArray = new int[elementsCount];
 int count = 0;
-for (int i = 0; i < myArray.Length; i++)
-{
-    Console.Write($"Введите положительное или отрицательное число под индексом: {i}:\t ");
-    myArray[i] = int.Parse(Console.ReadLine());
-    if (myArray[i] > 0) count++;
-}
+FillArray(myArray);
+Counting(myArray);
 Console.WriteLine($"Количество положительных чисел в массиве = {count}");
+
+void FillArray(int[] myArray)
+{
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        Console.Write($"Введите положительное или отрицательное число под индексом: {i}:\t ");
+        myArray[i] = int.Parse(Console.ReadLine());
+    }
+}
+void Counting(int[] myArray)
+{
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        if (myArray[i] > 0) count++;
+    }
+}
